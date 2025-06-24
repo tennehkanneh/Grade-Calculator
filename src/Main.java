8520d84dccc0fa9930a4c4c51fb6cc9dc8303925
Tenneh Kanneh <tennehkannehofficial@gmail.com>
2025-06-24 14:12:59 -0600

    Updating gitignore file to include files that end with `.class` or `.json`. Adding all the content that will be present on this page

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,55 @@
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        GradeCalculatorPane pane = new GradeCalculatorPane();
-
-        Scene scene = new Scene(pane, 900, 650);
-        
-
-        primaryStage.setTitle("Grade Calculator");
-        primaryStage.setScene(scene);
-        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-        primaryStage.show();
+    private static Scene scene;
+  
+    public static void main(String[] args) {
+        launch();
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage stage) throws IOException {
+        StartPane initialPane = new StartPane();
+        scene = new Scene(initialPane, 900, 650);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        stage.setTitle("Grade Calculator");
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void setRoot(String paneType) {
+
+        try {
+            Parent newRoot = null;
+            switch (paneType.toLowerCase()) {
+                case "start":
+                    newRoot = new StartPane();
+                    break;
+
+                case "class info":
+                    newRoot = new ClassInfoPane();
+                    break;
+
+                default:
+                    System.err.println("Unknown pane type: " + paneType);
+                    return;
+            }
+
+            if (newRoot != null) {
+                scene.setRoot(newRoot);
+                System.out.println("New Scene: " + newRoot);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Unable to Set New Scene");
+        }
     }
 }
---
+++ src/StartPane.java
@@ -0,0 +1,73 @@
+import javafx.event.ActionEvent;
+import javafx.geometry.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
+import javafx.stage.Stage;
+
+public class StartPane extends GridPane {
+    Text title;
+    Text author;
+    Button startButton;
+
+    public StartPane() {
+
+        ColumnConstraints oneColumn = new ColumnConstraints();
+        oneColumn.setHgrow(Priority.ALWAYS);
+        this.getColumnConstraints().add(oneColumn);
+
+        RowConstraints rowZero = new RowConstraints();
+        rowZero.setVgrow(Priority.ALWAYS);
+        rowZero.setValignment(VPos.CENTER);
+
+        RowConstraints rowOne = new RowConstraints();
+        rowOne.setPrefHeight(110);
+        rowOne.setValignment(VPos.BOTTOM);
+
+        this.getRowConstraints().addAll(rowZero, rowOne);
+
+        title = new Text("Grade Calculator");
+        title.setFont(Font.font("American Typewriter", FontWeight.BOLD, 60));
+
+        author = new Text("Created by Tenneh Kanneh");
+        author.setFont(new Font("Courier New", 25));
+        author.setFill(Color.color(0.0, 0.0, 0.0, 0.4));
+
+        VBox titleAndAuthor = new VBox();
+        titleAndAuthor.getChildren().addAll(title, author);
+        titleAndAuthor.setAlignment(Pos.CENTER);
+
+        this.add(titleAndAuthor, 0, 0);
+        GridPane.setHalignment(titleAndAuthor, HPos.CENTER);
+
+        startButton = new Button("Start Calculating Your Grade");
+        startButton.setFont(new Font("American Typewriter", 16));
+        startButton.getStyleClass().add("button");
+
+        this.add(startButton, 0, 1);
+        GridPane.setHalignment(startButton, HPos.CENTER);
+        GridPane.setValignment(startButton, VPos.TOP);
+
+        startButton.setOnAction(arg0 -> {
+            try {
+                startButtonPress(arg0);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        });
+
+        
+        this.setPadding(new Insets(20));
+
+        this.setAlignment(Pos.CENTER);
+    }
+
+    private void startButtonPress(ActionEvent event) throws Exception {
+        Main.setRoot("class info");
+    }
+
+}
